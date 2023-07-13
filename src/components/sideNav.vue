f5d2e45e14b91e689c1e9e59c0597fa9d93ddfa6
man-myth <manmeets1100@gmail.com>
2023-07-13 17:20:43 +0800

    add side nav bar

--- src/components/sideNav.vue
+++ src/components/sideNav.vue
@@ -1,12 +1,23 @@
 <template>
     <div class="col s3">
         <ul id="slide-out" class="sidenav sidenav-fixed">
-
+            <li>
+                <div class="col s12">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="public/images/profile-pic.jpg" class="circle">
+                        </div>
+                        <div class="card-title center-align">Juan Dela Cruz</div>
+                    </div>
+                </div>
+            </li>
             <li></li>
             <li><a href="#!"><i class="material-icons">dashboard</i>Dashboard</a></li>
             <li><a href="#!"><i class="material-icons">person</i>Clients</a></li>
             <li><a href="#!"><i class="material-icons">description</i>Loans</a></li>
-            <li><div class="divider"></div></li>
+            <li>
+                <div class="divider"></div>
+            </li>
             <li><a href="#!"><i class="material-icons">notifications</i>Notifications</a></li>
             <li><a href="#!"><i class="material-icons">settings</i>Settings</a></li>
         </ul>
@@ -21,3 +32,10 @@ import { onMounted } from 'vue';
     //     // M.Sidenav.getInstance(elems)
     // })
 </script>
+<style>
+
+#slide-out{
+    background-color: #E9B81E;
+}
+
+</style>
