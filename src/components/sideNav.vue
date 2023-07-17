67b6531d70d0d1d10d3b09a1c3bbe7392af550ba
man-myth <manmeets1100@gmail.com>
2023-07-17 17:20:37 +0800

    ...

--- src/components/sideNav.vue
+++ src/components/sideNav.vue
@@ -2,20 +2,18 @@
     <div class="">
         <ul id="slide-out" class="sidenav sidenav-fixed">
             <li>
-                <div class="card-panel">
-                    <div class="">
+                <div class="profile">
+                    <div class="profile-avatar">
                         <ProfileAvatar image="src/assets/images/profile-pic.jpeg" customSize="150px"></ProfileAvatar>
                     </div>
-                    <div class="card center-align">Juan Dela Cruz</div>
+                    <div class="flow-text center-align">Juan Dela Cruz</div>
                 </div>
-            </li>
+            </li >
             <li></li>
             <li><router-link to="/"><i class="material-icons">dashboard</i>Dashboard</router-link></li>
             <li><router-link to="/clients"><i class="material-icons">person</i>Clients</router-link></li>
             <li><router-link to="/loans"><i class="material-icons">description</i>Loans</router-link></li>
-            <li>
-                <div class="divider"></div>
-            </li>
+            <li></li>
             <li><router-link to=""><i class="material-icons">notifications</i>Notifications</router-link></li>
             <li><router-link to=""><i class="material-icons">settings</i>Settings</router-link></li>
         </ul>
@@ -35,10 +33,30 @@ export default {
 
 
 <style>
+.sidenav{
+    color: white;
+}
+.sidenav li>a {
+    color: white;
+    /* display: flex;
+    justify-content: center; */
+    margin-left: 2.4rem;
+    /* pading-left: 2.4rem; */
+
+}
+.sidenav li>a>i.material-icons {
+    color: white;
+}
 #slide-out {
     background-color: #E9B81E;
 }
-.card img{
-    
+.profile{
+    margin-top: 3rem;
+    height: 15rem;
+}
+.profile-avatar{
+    display: flex;
+    align-items: center;
+    justify-content: center;
 }
 </style>
